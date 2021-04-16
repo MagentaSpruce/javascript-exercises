70692a632d8992bb7a0982df2b70d2b079901cbe
Smyrna <81999424+MagentaSpruce@users.noreply.github.com>
2021-04-15 20:43:22 -0400

    Update palindromes.js
    Proposed solution to blank template.

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+  const chkStr = str.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+	let charCount = 0;
+    if(chkStr === "") {
+		
+		return false;
+	}
+    if ((chkStr.length) % 2 === 0) {
+		charCount = (chkStr.length) / 2;
+	} else {
+        if (chkStr.length === 1) {
+			
+			return true;
+		} else {
+            charCount = (chkStr.length - 1) / 2;
+		}
+	}
+    for (let i = 0; i < charCount; i++) {
+        if (chkStr[i] != chkStr .slice(-1-i)[0]) {
+			
+			return false;
+		}
+	}
+	
+	return true;
 }
 
 module.exports = palindromes
